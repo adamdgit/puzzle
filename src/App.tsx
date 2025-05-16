6fabd75552df7faf428b287c989803b2ced5accd
adamdgit <adammdemol@outlook.com>
2025-05-16 18:54:50 +1000

    Update css variables using js values

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,23 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import CroppingTool from './components/CroppingTool';
 import GameBoard from './components/GameBoard';
 
 function App() {
   const [croppedImage, setCroppedImage] = useState<HTMLCanvasElement | null>(null);
-  const [BOARDSIZE] = useState(440);
-  const [ROWS] = useState(4);
-  const [COLUMNS] = useState(4);
+
+  const [BOARDSIZE] = useState(400);
+  const [TILESIZE] = useState(100);
+  const [ROWS] = useState(BOARDSIZE / TILESIZE);
+  const [COLUMNS] = useState(BOARDSIZE / TILESIZE);
+
+  useEffect(() => {
+    // Set the CSS variables to ensure they align with javascript state
+    const root = document.querySelector(':root') as HTMLElement;
+    if (!root) return
+
+    root.style.setProperty('--boardsize', `${ROWS}`);
+    root.style.setProperty('--tilesize', `${TILESIZE}px`);
+  },[ROWS, TILESIZE]);
 
   return (
     <main>
